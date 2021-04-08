b396a77c25172aeca548fb10b5b4d871d15d0469
Tanya00793 <nusha00793@yandex.ru>
2021-04-08 23:04:21 +0300

    Correction-13 (Fixed after review)

--- scripts/index.js
+++ scripts/index.js
@@ -1,145 +1,98 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const profile = document.querySelector('.profile');
 const photoGrid = document.querySelector('.photo-grid');
 
-const editProfileModal = document.querySelector('.edit-profile-modal');
-const addCardModal = document.querySelector('.add-card-modal');
-const reviewCardModal = document.querySelector('.review-card-modal');
+const modal = document.querySelector('.modal');
+const modalEditProfile = document.querySelector('.modal-edit-profile');
+const modalAddCard = document.querySelector('.modal-add-card');
+const modalPreviewCard = document.querySelector('.modal-preview-card');
 
-const editProfileModalForm = document.forms['editProfileModalForm'];
-const openEditProfileModalBtn = document.querySelector('.profile__edit-button');
-const submitEditProfileModalBtn = document.querySelector('.edit-profile-modal__submit-button');
-const closeEditProfileModalBtn = document.querySelector('.edit-profile-modal__close-button');
-const closeReviewCardModalBtn = document.querySelector('.review-card-modal__close-button');
+const modalEditProfileForm = document.forms['modalEditProfileForm'];
+const modalEditProfileOpenBtn = document.querySelector('.profile__edit-button');  
+const modalEditProfileCloseBtn = document.querySelector('.modal-edit-profile__close-button');
+const modalPeviewCardCloseBtn = document.querySelector('.modal-preview-card__close-button');
 
-const addCardModalForm = document.forms['addCardForm'];
-const openAddCardModalBtn = document.querySelector('.profile__add-card-button');
-const closeAddCardModalBtn = document.querySelector('.add-card-modal__close-button');
+const modalAddCardForm = document.forms['addCardForm'];
+const modalAddCardOpenBtn = document.querySelector('.profile__add-card-button');
+const modalAddCardCloseBtn = document.querySelector('.modal-add-card__close-button');
 
-const cardsTemplate = photoGrid.querySelector('.cards-template').content.querySelector('.card');
+const cardTemplate = photoGrid.querySelector('.card-template').content.querySelector('.card');
 
-const nameInput = editProfileModalForm.querySelector('#nameInput');
-const professionInput = editProfileModalForm.querySelector('#professionInput');
-const placeTitleInput = addCardModalForm.querySelector('#placeTitleInput');
-const placeLinkInput = addCardModalForm.querySelector('#placeLinkInput');
-const placeTitleCardReview = reviewCardModal.querySelector('#placeTitleReviewCard');
+const nameInput = modalEditProfileForm.querySelector('#nameInput');
+const professionInput = modalEditProfileForm.querySelector('#professionInput');
+const placeTitleInput = modalAddCardForm.querySelector('#placeTitleInput');
+const placeLinkInput = modalAddCardForm.querySelector('#placeLinkInput');
 
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const cardTitle = document.querySelector('.card__title');
-const cardImageLink = document.querySelector('.card__image');
 
 
 //---------------------------------------------------------------
 
 
-const cardsArr = initialCards.forEach(function(item) {
-  photoGrid.append(initialCard(item.name, item.link));
-});
-
 function initialCard(titleValue, linkValue) {
-  const cards = cardsTemplate.cloneNode(true);
-  cards.querySelector('.card__title').textContent = titleValue;
-  cards.querySelector('.card__image').src = linkValue;
-  cards.querySelector('.card__title').alt = titleValue;
-  const cardReviewImage = cards.querySelector('.card__image');
-  cardReviewImage.addEventListener('click', function(){
-    openReviewCardModal();
-    const reviewCardModalImage = document.querySelector('.review-card-modal__image');
-    reviewCardModalImage.src = cardReviewImage.src; 
-    const reviewCardPlaceTitle = document.querySelector('.review-card-modal__title');
-    reviewCardPlaceTitle.textContent = cards.querySelector('.card__title').textContent;
+  const card = cardTemplate.cloneNode(true);
+  card.querySelector('.card__title').textContent = titleValue;
+  card.querySelector('.card__image').src = linkValue;
+  card.querySelector('.card__title').alt = titleValue;
+  const cardPreviewImage = card.querySelector('.card__image');
+  cardPreviewImage.addEventListener('click', function(){
+    openModal(modalPreviewCard);
+    const modalPreviewCardImage = document.querySelector('.modal-preview-card__image');
+    modalPreviewCardImage.src = cardReviewImage.src; 
+    const previewCardPlaceTitle = document.querySelector('.modal-preview-card__title');
+    previewCardPlaceTitle.textContent = card.querySelector('.card__title').textContent;
   });
-  const likeCardBtn = cards.querySelector('.card__like-button');
-  likeCardBtn.addEventListener('click', function() {
-    likeCardBtn.style.backgroundImage = `url('images/__like-button-active.svg')`;
+  const cardLikeBtn = card.querySelector('.card__like-button');
+  cardLikeBtn.addEventListener('click', function() {
+    cardLikeBtn.classList.toggle('card__like-button_active');
   });
-  const deleteCardBtn = cards.querySelector('.card__delete-button');
-  deleteCardBtn.addEventListener('click', function() {
-    cards.remove();
+  const cardDeleteBtn = card.querySelector('.card__delete-button');
+  cardDeleteBtn.addEventListener('click', function() {
+    card.remove();
   });
-  return(cards);
+  return(card);
 };
 
-function openEditProfileModal() {
-  nameInput.value = profileName.textContent;
-  professionInput.value = profileProfession.textContent;
-  editProfileModal.classList.add('edit-profile-modal_opened');
+
+function openModal (modal) {
+  modal.classList.add('modal_opened');
 };
 
-function openAddCardModal () {
-  addCardModal.classList.add('add-card-modal_opened');
+function closeModal (modal) {
+  modal.classList.remove('modal_opened');
 };
 
-function openReviewCardModal () {
-  reviewCardModal.classList.add('review-card-modal_opened');
-};
 
-function closeEditProfileModal () {
-  editProfileModal.classList.remove('edit-profile-modal_opened');
-};
-
-function closeAddCardModal () {
-  addCardModal.classList.remove('add-card-modal_opened');
-};
-
-function closeReviewCardModal () {
-  reviewCardModal.classList.remove('review-card-modal_opened');
-};
-
-const editProfileSubmitHandler = e => {
+const modalProfileEditSubmitHandler = e => {
 	e.preventDefault(); 
   profileName.textContent = nameInput.value;
   profileProfession.textContent = professionInput.value;
-  closeEditProfileModal();
-  editProfileModalForm.reset;
+  closeModal(modalEditProfile);
+  modalEditProfileForm.reset();
 };
 
-const addCardSubmitHandler = e => {
+const modalAddCardSubmitHandler = e => {
 	e.preventDefault();
   const placeTitleInputValue = placeTitleInput.value;
   const placeLinkInputValue = placeLinkInput.value;
   photoGrid.prepend(initialCard(placeTitleInputValue, placeLinkInputValue));
-  closeAddCardModal();
-  addCardModalForm.reset;
+  closeModal(modalAddCard);
+  modalAddCardForm.reset();
 };
 
 
 //---------------------------------------------------------------
 
+modalEditProfileOpenBtn.addEventListener('click', ()=> {
+  nameInput.value = profileName.textContent;
+  professionInput.value = profileProfession.textContent;
+  openModal(modalEditProfile);
+});
+modalEditProfileCloseBtn.addEventListener('click', () => closeModal(modalEditProfile));
+modalEditProfileForm.addEventListener('submit', modalProfileEditSubmitHandler);
 
-openEditProfileModalBtn.addEventListener('click', openEditProfileModal);
-closeEditProfileModalBtn.addEventListener('click', closeEditProfileModal);
-editProfileModalForm.addEventListener('submit', editProfileSubmitHandler);
+modalAddCardOpenBtn.addEventListener('click', () => openModal(modalAddCard));
+modalAddCardCloseBtn.addEventListener('click', () => closeModal(modalAddCard));
+modalAddCardForm.addEventListener('submit', modalAddCardSubmitHandler);
 
-openAddCardModalBtn.addEventListener('click', openAddCardModal);
-closeAddCardModalBtn.addEventListener('click', closeAddCardModal);
-addCardModalForm.addEventListener('submit', addCardSubmitHandler);
-
-closeReviewCardModalBtn.addEventListener('click', closeReviewCardModal);
+modalPeviewCardCloseBtn.addEventListener('click', () => closeModal(modalPreviewCard));
